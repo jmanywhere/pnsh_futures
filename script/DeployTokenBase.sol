268ef5b4060aa002bf06b7d5815d3ff453d9408a
Plastic Digits <plasticdigits@protonmail.com>
2023-10-26 11:59:45 +0900

    Add deploy script for ptoken base

---
+++ script/DeployTokenBase.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.8.19;
+
+import "forge-std/Script.sol";
+import "../src/TokenBase.sol";
+
+contract DeployTokenBase is Script {
+    string _name = "Nebula";
+    string _ticker = "NSH";
+    uint256 _initialSupply = 0 ether;
+    bool _isMintable = false;
+    bool _isTaxable = false;
+
+    function run() public {
+        vm.startBroadcast();
+        new TokenBase(_name, _ticker, _initialSupply, _isMintable, _isTaxable);
+        vm.stopBroadcast();
+    }
+}
